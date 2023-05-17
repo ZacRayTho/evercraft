efee71a389bbb0752c1a6aa77e4ebd898c8aa0c1
ZacRayTho <zachary.thomas@student.awesomeinc.org>
2023-05-17 21:34:08 +0000

    trying to figure out how to recheck variable on attribute change

--- src/character.py
+++ src/character.py
@@ -1,8 +1,38 @@
 class Character():
-    name = ""
-    armor = 10
+    str = 10
+    dex = 10
+    con = 10
+    wis = 10
+    int = 10
+    cha = 10
+
+    modifiers = [-5,-4,-4,-3,-3,-2,-2,-1,-1,0,0,1,1,2,2,3,3,4,4,5]
+
+    name = "Default Dan"
+    alignment = 'Neutral'
+    alive = True
+    armor = 10 + modifiers[dex - 1]
     hp = 5
 
     def __init__(self, **attr):
         self.__dict__.update(attr)
 
+    def attack(self, roll, enemy):
+        if roll == 20:
+            damage = ((1 + (self.modifiers[self.str - 1] * 2)) * 2)
+            enemy.hp -= damage if damage > 0 else 1
+            enemy.death_check()
+            return True
+        elif roll + self.modifiers[self.str - 1] >= enemy.armor:
+            damage = 1 + self.modifiers[self.str - 1]
+            enemy.hp -= damage if damage > 0 else 1
+            enemy.death_check()
+            return True
+
+    def death_check(self):
+        if self.hp <= 0:
+            self.alive = False
+
+c = Character()
+c.dex = 12
+print(c.armor)
--- src/tests/test_sample.py
+++ src/tests/test_sample.py
@@ -4,23 +4,205 @@ from character import Character
 # Tests
 # Create a Character
 def test_create_character():
-    c = Character(name='Buddy', alignment='Good', armor="10")
+    c = Character()
     assert isinstance(c, Character)
 
 # Character has a name
 def test_character_name():
-    c = Character(name='Buddy', alignment='Good', armor=10)
+    c = Character()
     assert hasattr(c, "name")
 
-# Character can set name
+# Character can get name
+def test_character_get_name():
+    c = Character()
+    assert c.name == 'Default Dan'
+
+# Character can set name after creation
 def test_character_set_name():
-    c = Character(name='Buddy', alignment='Good', armor=10)
+    c = Character()
+    c.name = 'Buddy'
     assert c.name == 'Buddy'
 
-#### Feature: Create a Character
-# > As a character I want to have a name so that I can be distinguished 
-# from other characters - multiple characters
-# - can get and set Name
+# Character can set name during creation
+def test_character_set_name_as_created():
+    c = Character(name='Buddy')
+    assert c.name == 'Buddy'
+
+# Character has alignment
+def test_character_alignment():
+    c = Character()
+    assert hasattr(c, 'alignment')
+
+# Character can set alignment
+def test_character_alignment_set():
+    c = Character(alignment='Good')
+    assert c.alignment == 'Good'    
+
+# Character has armor class
+def test_character_armor():
+    c = Character()
+    assert hasattr(c, "armor")
+
+# Character armor defaults to 10
+def test_character_armor_default():
+    c = Character()
+    assert c.armor == 10
+
+# Character has health points (hp)
+def test_character_hp():
+    c = Character()
+    assert hasattr(c, "hp")
+
+# Character hp defaults to 5
+def test_character_hp_default():
+    c = Character()
+    assert c.hp == 5
+
+# Character can attack
+def test_character_attack():
+    c = Character()
+    assert callable(getattr(c, "attack"))
+
+# Character attack roll meets enemy (armor class) AC is a hit
+def test_character_attack_roll_meet_ac():
+    c = Character()
+    z = Character()
+    assert c.attack(10, z)
+
+# Character attack roll exceeds enemy AC is a hit
+def test_character_attack_roll_exceed_ac():
+    c = Character()
+    z = Character() 
+    assert c.attack(19, z)
+
+# Character attack roll under enemy AC is a miss
+def test_character_attack_roll_under_ac():
+    c = Character()
+    z = Character()
+    assert not c.attack(3, z)
+
+# Character takes 1 damage on successful attack roll
+def test_character_damage():
+    c = Character()
+    z = Character()
+    c.attack(12, z)
+    assert z.hp == 4
+
+# Character takes double damage on attack roll of 20
+def test_character_damage_nat20():
+    c = Character()
+    z = Character()
+    c.attack(20, z)
+    assert z.hp == 3
+
+# Character is dead when hp are 0 or less
+def test_character_death():
+    c = Character()
+    z = Character()
+    c.attack(20, z)
+    c.attack(20, z)
+    c.attack(20, z)
+    assert z.alive == False
+
+# Character has Strength ability
+def test_character_strength():
+    c = Character()
+    assert hasattr(c, 'str')
+
+# Character has Dexterity ability
+def test_character_dexterity():
+    c = Character()
+    assert hasattr(c, 'dex')
+
+# Character has Constitution ability
+def test_character_constitution():
+    c = Character()
+    assert hasattr(c, 'con')
+
+# Character has Wisdom ability
+def test_character_wisdom():
+    c = Character()
+    assert hasattr(c, 'wis')
+
+# Character has Intelligence ability
+def test_character_intelligence():
+    c = Character()
+    assert hasattr(c, 'int')
+
+# Character has Charisma ability
+def test_character_charisma():
+    c = Character()
+    assert hasattr(c, 'cha')
+
+# Character Abilities range can't go below 1-------------------------------------------------------
+# Character Abilities range can't go above 20
+
+# Character has Strength ability
+def test_character_strength_default():
+    c = Character()
+    assert c.str == 10
+
+# Character has Dexterity ability
+def test_character_dexterity_default():
+    c = Character()
+    assert c.dex == 10
+
+# Character has Constitution ability
+def test_character_constitution_default():
+    c = Character()
+    assert c.con == 10
+
+# Character has Wisdom ability
+def test_character_wisdom_default():
+    c = Character()
+    assert c.wis == 10
+
+# Character has Intelligence ability
+def test_character_intelligence_default():
+    c = Character()
+    assert c.int == 10
+
+# Character has Charisma ability
+def test_character_charisma_default():
+    c = Character()
+    assert c.cha == 10
+
+# Character's strength modifier is added to attack roll
+def test_character_str_mod_attack():
+    c = Character()
+    z = Character()
+    c.str = 20
+    assert c.attack(5, z)
+
+# Character's strength modifier is added to attack roll
+def test_character_str_mod_damage():
+    c = Character()
+    z = Character()
+    c.str = 12
+    c.attack(10, z)
+    assert z.hp == 3
+
+# Character's strength modifier is doubled on critical hit
+def test_character_str_mod_crit():
+    c = Character()
+    z = Character()
+    c. str = 12
+    c.attack(20,z)
+    assert z.hp == -1
+
+# Character's minimum attack always 1
+def test_character_str_mod_min_attack():
+    c = Character()
+    z = Character()
+    c. str = 1
+    c.attack(20,z)
+    assert z.hp == 4
+
+# Character's Dexterity modifier is added to armor
+def test_character_dex_mod_armor():
+    c = Character()
+    c.dex = 12
+    assert c.armor == 11
 
 
 
